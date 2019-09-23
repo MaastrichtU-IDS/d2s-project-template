5721b4aaebe004f88013c10c5ada3e4cbe66275e
vemonet <vincent.emonet@gmail.com>
2019-09-24 01:36:33 +0200

    create new folder structure for stitch

---
+++ datasets/stitch/download/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Stitch: http://stitch.embl.de/cgi/download.pl
+
+# Chemical-chemical links
+wget -N https://github.com/MaastrichtU-IDS/data2services-download/raw/master/datasets/stitch-sample/chemical_chemical.links.v5.0.tsv.gz
+
+
+# Protein-chemical links. Taking only for human specie at the moment to make it lighter (too big otherwise)
+wget -N https://github.com/MaastrichtU-IDS/data2services-download/raw/master/datasets/stitch-sample/9606.protein_chemical.links.detailed.v5.0.tsv.gz
+
+
+find . -name "*.gz" -exec gzip -d  {} +
