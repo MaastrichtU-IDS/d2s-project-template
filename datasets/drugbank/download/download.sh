c900e7535cdf4d7129744af399a8d981bba75bbd
Vincent Emonet <vincent.emonet@gmail.com>
2019-10-18 18:35:34 +0200

    fix drugbank download

--- datasets/drugbank/download/download.sh
+++ datasets/drugbank/download/download.sh
@@ -5,9 +5,12 @@ wget -N https://github.com/MaastrichtU-IDS/data2services-download/raw/master/dat
 # Download providing user login and password
 #curl -Lfv -o drugbank.zip -u $USERNAME:$PASSWORD https://www.drugbank.ca/releases/5-1-1/downloads/all-full-database
 
+unzip *.zip -d .
+
 # Unzip all files in subdir with name of the zip file
-find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`/${filename%.*}" "$filename"; done;
-echo "content of drugbank"
-ls drugbank
-mv drugbank/* .
-rmdir drugbank
+# find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`/${filename%.*}" "$filename"; done;
+echo "content of dir"
+ls
+#ls drugbank
+#mv drugbank/* .
+# rmdir drugbank
