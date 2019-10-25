d1a42567e3859697577337e4c1e496442bd9ba9d
Vincent Emonet <vincent.emonet@gmail.com>
2019-10-25 18:24:17 +0200

    change port and virtual host of virtuoso to make it work on node1

--- restart_virtuoso.sh
+++ restart_virtuoso.sh
@@ -2,5 +2,6 @@
 docker-compose -f d2s-cwl-workflows/docker-compose.yaml down
 rm -rf /data/d2s-workspace/virtuoso
 docker-compose -f d2s-cwl-workflows/docker-compose.yaml up -d --build --force-recreate virtuoso drill
-sleep 5
+sleep 2
+sudo chown -R ${USER}:${USER} /data/d2s-workspace
 cp d2s-cwl-workflows/support/virtuoso/load.sh /data/d2s-workspace/virtuoso
