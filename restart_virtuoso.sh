ef2e1af9112212c09fb287078f8a2e17e6ca5a3a
Vincent Emonet <vincent.emonet@gmail.com>
2020-01-21 18:39:27 +0100

    update restart virtuoso to add echo

--- restart_virtuoso.sh
+++ restart_virtuoso.sh
@@ -3,5 +3,7 @@ docker-compose -f d2s-cwl-workflows/docker-compose.yaml down
 rm -rf /data/d2s-workspace/virtuoso
 docker-compose -f d2s-cwl-workflows/docker-compose.yaml up -d --build --force-recreate virtuoso drill
 sleep 2
+
+echo "Asking password to sudo to change ownership of workspace (chown)"
 sudo chown -R ${USER}:${USER} /data/d2s-workspace
 cp d2s-cwl-workflows/support/virtuoso/load.sh /data/d2s-workspace/virtuoso
