2fa247832e994998cc5e61a8f8c94806024ee04c
Vincent Emonet <vincent.emonet@gmail.com>
2020-01-21 19:25:25 +0100

    rename restart_virtuoso.sh to restart_d2s_services.sh and add graphd and browser for virtuoso

--- restart_d2s_services.sh
+++ restart_d2s_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker-compose -f d2s-cwl-workflows/docker-compose.yaml down
 rm -rf /data/d2s-workspace/virtuoso
-docker-compose -f d2s-cwl-workflows/docker-compose.yaml up -d --build --force-recreate virtuoso drill
+docker-compose -f d2s-cwl-workflows/docker-compose.yaml up -d --build --force-recreate virtuoso drill graphdb browse-local-virtuoso
 sleep 2
 
 echo "Asking password to sudo to change ownership of workspace (chown)"
