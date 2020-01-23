f5b3e5fd3f6df7af66bc869361a2e769cb4e207f
Vincent Emonet <vincent.emonet@gmail.com>
2020-01-23 08:06:02 +0100

    fix restart_d2s_services.sh by removing

--- restart_d2s_services.sh
+++ restart_d2s_services.sh
@@ -12,7 +12,7 @@ sleep 2
 
 # Change permissions to current user and group id (tested for Ubuntu and MacOS)
 echo "Asking password to sudo to change ownership of workspace (chown)"
-sudo chown -R ${UID}:${GID} /data/d2s-workspace
+sudo chown -R ${UID} /data/d2s-workspace
 
 # Copy the load.sh script used for Virtuoso bulk load
 cp d2s-cwl-workflows/support/virtuoso/load.sh /data/d2s-workspace/virtuoso
