889c0b8df00142e49c2e0f2640dfc570742e0473
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-23 07:50:28 +0100

    add RML mappings for geonames and worldbank

---
+++ datasets/geonames/download/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Download TSV file from GitHub
+wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/UM_KEN4256_KnowledgeGraphs/master/dataset-geonames-countryInfo.csv
---
+++ datasets/worldbank/download/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## Download TSV file from GitHub
+wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/UM_KEN4256_KnowledgeGraphs/master/dataset-worldbank-gdp-full.xml
+wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/UM_KEN4256_KnowledgeGraphs/master/dataset-worldbank-gdp.xml
