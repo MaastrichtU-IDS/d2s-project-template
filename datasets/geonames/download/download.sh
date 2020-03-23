2eb2b48ee19345cecca2e7819f77357dcea0c9bd
Vincent Emonet <vincent.emonet@gmail.com>
2020-03-23 10:10:54 +0100

    change the worldbank and geonames sparql endpoint to geoeconomics

--- datasets/geonames/download/download.sh
+++ datasets/geonames/download/download.sh
@@ -2,3 +2,4 @@
 
 # Download TSV file from GitHub
 wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/UM_KEN4256_KnowledgeGraphs/master/dataset-geonames-countryInfo.csv
+wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/d2s-scripts-repository/master/resources/map-country-iso2-iso3.csv
