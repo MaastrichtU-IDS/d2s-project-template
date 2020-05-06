a6449b608b46d0960b003e544be2dfc0ed8159d6
Vincent Emonet <vincent.emonet@gmail.com>
2020-05-06 09:00:25 +0200

    fix drugbank

--- datasets/drugbank/download/download.sh
+++ datasets/drugbank/download/download.sh
@@ -3,21 +3,22 @@
 USERNAME="vincent.emonet@maastrichtuniversity.nl"
 PASSWORD="password"
 
-# Use sample hosted on GitHub, faster for testing
-wget -N https://github.com/MaastrichtU-IDS/d2s-scripts-repository/raw/master/resources/drugbank-sample/drugbank.zip
-
-# See the uncompressed file here: 
-# https://github.com/MaastrichtU-IDS/d2s-scripts-repository/blob/master/resources/drugbank-sample/drugbank.xml
-
 # Download full DrugBank dataset providing user login and password
-#curl -Lfv -o drugbank.zip -u $USERNAME:$PASSWORD https://www.drugbank.ca/releases/5-1-6/downloads/all-full-database
+curl -Lfv -o drugbank.zip -u $USERNAME:$PASSWORD https://www.drugbank.ca/releases/5-1-6/downloads/all-full-database
 
 # Download OMOP to DrugBank mappings
 wget -N https://github.com/OHDSI/KnowledgeBase/blob/master/LAERTES/terminology-mappings/RxNORM-to-UNII-PreferredName-To-DrugBank/rxnorm-drugbank-omop-mapping-CLEANED.tsv
 
+# Use DrugBank XML sample hosted on GitHub, faster for testing
+# wget -N https://github.com/MaastrichtU-IDS/d2s-scripts-repository/raw/master/resources/drugbank-sample/drugbank.zip
+# See the uncompressed file here: 
+# https://github.com/MaastrichtU-IDS/d2s-scripts-repository/blob/master/resources/drugbank-sample/drugbank.xml
+
 unzip *.zip -d .
 
 mv "full database.xml" "drugbank_database.xml"
+
+chmod 777 *
 
 # Unzip all files in subdir with name of the zip file
 # find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`/${filename%.*}" "$filename"; done;
