d875c5cd4c355561b05f1a0e2dc701d92487abb0
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-13 23:55:15 +0200

    update hgnc import

--- datasets/hgnc/download/download.sh
+++ datasets/hgnc/download/download.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 ## Download sample TSV files from GitHub (OMOP CDM mappings, about 15M)
-wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/d2s-scripts-repository/master/resources/cohd-sample/concepts.tsv
-
-
-# UNTAR recursively all .tar.gz files in current dir
-find . -name "*.tar.gz" -exec tar -xzvf {} \;
-
-# UNZIP files
-unzip -o \*.zip
+wget -N ftp://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
 
 ## RENAME EXTENSION (e.g.: txt in tsv)
-# rename s/\.txt/.tsv/ *.txt
+rename s/\.txt/.tsv/ *.txt
 
 # Convert TSV to CSV for RMLStreamer
-sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' concepts.tsv > concepts.csv
+sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' hgnc_complete_set.tsv > hgnc_complete_set.csv
 
 # Make sure right permissions are set properly
 chmod 777 *
