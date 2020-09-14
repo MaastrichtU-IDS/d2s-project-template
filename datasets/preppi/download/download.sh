6b5bc7e9d67f58dbb2326e362cd10b07c9c5b375
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-14 20:34:31 +0200

    fix preppi ddl

--- datasets/preppi/download/download.sh
+++ datasets/preppi/download/download.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 ## Download sample TSV files from GitHub (OMOP CDM mappings, about 15M)
-wget -O preppi.tsv https://honiglab.c2b2.columbia.edu/PrePPI/ref/preppi_final600.txt.tar.gz
+wget -O preppi.tsv.tar.gz https://honiglab.c2b2.columbia.edu/PrePPI/ref/preppi_final600.txt.tar.gz
 
 
 # UNTAR recursively all .tar.gz files in current dir
-find . -name "*.tar.gz" -exec tar -xzvf {} \;
-
-## RENAME EXTENSION (e.g.: txt in tsv)
-rename s/\.txt/.tsv/ *.txt
+tar -xzvf *.tar.gz
 
 # Convert TSV to CSV for RMLStreamer
 sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' preppi.tsv > preppi.csv
