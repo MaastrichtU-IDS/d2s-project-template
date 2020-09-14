1573e99493bb8233469e2c60d701f07221e3e019
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-14 08:43:46 +0200

    improve mappings

--- datasets/stitch/download/download.sh
+++ datasets/stitch/download/download.sh
@@ -3,11 +3,15 @@
 # Stitch: http://stitch.embl.de/cgi/download.pl
 
 # Chemical-chemical links
-wget -N https://github.com/MaastrichtU-IDS/d2s-download/raw/master/datasets/stitch-sample/chemical_chemical.links.v5.0.tsv.gz
+wget -N http://stitch.embl.de/download/chemical_chemical.links.v5.0.tsv.gz
 
 
 # Protein-chemical links. Taking only for human specie at the moment to make it lighter (too big otherwise)
-wget -N https://github.com/MaastrichtU-IDS/d2s-download/raw/master/datasets/stitch-sample/9606.protein_chemical.links.detailed.v5.0.tsv.gz
+wget -N http://stitch.embl.de/download/protein_chemical.links.detailed.v5.0.tsv.gz
+# 32G
+
+# Only Human, 100M:
+http://stitch.embl.de/download/protein_chemical.links.detailed.v5.0/9606.protein_chemical.links.detailed.v5.0.tsv.gz
 
 
 find . -name "*.gz" -exec gzip -d  {} +
