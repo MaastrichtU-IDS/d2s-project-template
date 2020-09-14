061aec86b32101bd21ef53fac67c8e011dd1d0eb
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-14 20:26:57 +0200

    fix huri ddl script

--- datasets/huri/download/download.sh
+++ datasets/huri/download/download.sh
@@ -3,5 +3,8 @@
 ## Download HuRI TSV file
 wget -O huri.tsv http://www.interactome-atlas.org/data/HuRI.tsv
 
+# Add header
+sed -i '1s/^/geneA\tgeneB\n/' huri.tsv
+
 # Convert TSV to CSV for RMLStreamer
 sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' huri.tsv > huri.csv
