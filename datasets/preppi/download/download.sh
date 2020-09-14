8ea599b91a538795d6ad2a0723086adc7d8b474b
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-14 20:47:07 +0200

    update workflow

--- datasets/preppi/download/download.sh
+++ datasets/preppi/download/download.sh
@@ -3,9 +3,7 @@
 ## Download sample TSV files from GitHub (OMOP CDM mappings, about 15M)
 wget -O preppi.tsv.tar.gz https://honiglab.c2b2.columbia.edu/PrePPI/ref/preppi_final600.txt.tar.gz
 
-
-# UNTAR recursively all .tar.gz files in current dir
-tar -xzvf *.tar.gz
+tar -xzvf preppi.tsv.tar.gz preppi.tsv
 
 # Convert TSV to CSV for RMLStreamer
 sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' preppi.tsv > preppi.csv
