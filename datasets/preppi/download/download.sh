32a017d778b71075736a2b99f59cf81b2a32710a
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-14 20:52:33 +0200

    fix preppi ddl

--- datasets/preppi/download/download.sh
+++ datasets/preppi/download/download.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-## Download sample TSV files from GitHub (OMOP CDM mappings, about 15M)
-wget -O preppi.tsv.tar.gz https://honiglab.c2b2.columbia.edu/PrePPI/ref/preppi_final600.txt.tar.gz
+## Download preppi_final600.txt
+wget -N https://honiglab.c2b2.columbia.edu/PrePPI/ref/preppi_final600.txt.tar.gz
 
-tar -xzvf preppi.tsv.tar.gz preppi.tsv
+tar -xzvf *.tar.gz
+
+rename s/\.txt/.tsv/ *.txt
 
 # Convert TSV to CSV for RMLStreamer
-sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' preppi.tsv > preppi.csv
+sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' preppi_final600.tsv > preppi.csv
 
 # Make sure right permissions are set properly
 chmod 777 *
