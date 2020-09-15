444944fadfee3944dda965bf9e9c0ed471e6b943
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-15 09:41:21 +0200

    add preppi dataset convertion with RML

--- datasets/hgnc/download/download.sh
+++ datasets/hgnc/download/download.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
 ## Download sample TSV files from GitHub (OMOP CDM mappings, about 15M)
-wget -N ftp://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
+# wget -N ftp://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
 
-## RENAME EXTENSION (e.g.: txt in tsv)
-rename s/\.txt/.tsv/ *.txt
+# ## RENAME EXTENSION (e.g.: txt in tsv)
+# rename s/\.txt/.tsv/ *.txt
+
+
+pip install pandas
+
+python3 <<HEREDOC
+import pandas as pd
+csv_table=pd.read_table('hgnc_complete_set.tsv',sep='\t')
+csv_table.applymap
+s = pd.Series(csv_table)
+s.str.strip()
+csv_table.to_csv('hgnc_complete_set.csv',index=False)
+HEREDOC
+
+# Use TSV in RML
+#sources:
+# access: source_1.tsv
+# referenceFormulation: csv
+# delimiter: \t
 
 # Convert TSV to CSV for RMLStreamer
 sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' hgnc_complete_set.tsv > hgnc_complete_set.csv
 
 # Make sure right permissions are set properly
-chmod 777 *
+# chmod 777 *
--- datasets/preppi/download/download.sh
+++ datasets/preppi/download/download.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-## Download preppi_final600.txt
+## Download preppi_final600.txt (30M compressed)
 wget -N https://honiglab.c2b2.columbia.edu/PrePPI/ref/preppi_final600.txt.tar.gz
 
+# Unzip
 tar -xzvf *.tar.gz
 
+# Rename .txt to .tsv
 rename s/\.txt/.tsv/ *.txt
 
+# Convert commas to | for ddbs column
+sed -e 's/,/|/g' preppi_final600.tsv > preppi_final600.tsv
+# Convert PubMed publications CURIEs to URIs
+sed -e 's/pubmed:/https:\/\/identifiers.org\/pubmed:/g' preppi_final600.tsv > preppi_final600.tsv
 # Convert TSV to CSV for RMLStreamer
 sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' preppi_final600.tsv > preppi.csv
 
-# Make sure right permissions are set properly
-chmod 777 *
+
+# prot1	prot2	str_score	protpep_score	str_max_score	red_score	ort_score	phy_score	coexp_score	go_score	total_score	dbs	pubs	exp_score	final_score
+# Q13131	P14625	18.59	6.44772	18.59	4.2492	0.6153	2.416	9.4687	10.8	12008.4				12008.4
+# P06400	Q96N96	1.8315	14.3222	14.3222	4.2492	0	2.416	2.1077	10.8	3346.93				3346.93
+# Q7Z6V5	Q8NCE0	4.5712	0	4.5712	0	0	1.5978	9.4687	24.11	1667.4				1667.4
+# P09661	Q13573	0	0	0	0	3.1392	0	1
+
+# "prot1","prot2","str_score","protpep_score","str_max_score","red_score","ort_score","phy_score","coexp_score","go_score","total_score","dbs","pubs","exp_score","final_score"
+# "Q13131","P14625","18.59","6.44772","18.59","4.2492","0.6153","2.416","9.4687","10.8","12008.4","","","","12008.4"
+# "Q9Y2X7","Q9NPJ6","0","2.59","2.59","1.0145","0","2.416","1.5818","0","10.0415","BGDB|","pubmed:25281560|pubmed:25281575","957.82","9617.95"
+# "Q9Y2X7","Q9NPJ6","0","2.59","2.59","1.0145","0","2.416","1.5818","0","10.0415","BGDB|","pubmed:25281560","957.82","9617.95"
