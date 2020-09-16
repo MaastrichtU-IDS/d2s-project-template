ccbf28d28a778d455c41d091f1f9af306ff52014
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-16 15:02:23 +0200

    Update HGNC for RML. delete old imported-uniprot and wikipathways

--- datasets/hgnc/download/download.sh
+++ datasets/hgnc/download/download.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
 ## Download sample TSV files from GitHub (OMOP CDM mappings, about 15M)
-# wget -N ftp://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
-
-# ## RENAME EXTENSION (e.g.: txt in tsv)
-# rename s/\.txt/.tsv/ *.txt
-
-
-pip install pandas
-
-python3 <<HEREDOC
-import pandas as pd
-csv_table=pd.read_table('hgnc_complete_set.tsv',sep='\t')
-csv_table.applymap
-s = pd.Series(csv_table)
-s.str.strip()
-csv_table.to_csv('hgnc_complete_set.csv',index=False)
-HEREDOC
-
-# Use TSV in RML
-#sources:
-# access: source_1.tsv
-# referenceFormulation: csv
-# delimiter: \t
+wget -N ftp://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
 
 # Convert TSV to CSV for RMLStreamer
-sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' hgnc_complete_set.tsv > hgnc_complete_set.csv
+sed -e 's/"//g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/' -e 's/\r//' hgnc_complete_set.txt > hgnc.csv
 
-# Make sure right permissions are set properly
-# chmod 777 *
+
+# pip install pandas
+
+# python3 <<HEREDOC
+# import pandas as pd
+# csv_table=pd.read_table('hgnc_complete_set.tsv',sep='\t')
+# csv_table.applymap
+# s = pd.Series(csv_table)
+# s.str.strip()
+# csv_table.to_csv('hgnc_complete_set.csv',index=False)
+# HEREDOC
