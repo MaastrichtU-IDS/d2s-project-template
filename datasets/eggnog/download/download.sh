f67e44554e59ba0d6d51921bf44dad72b445f3f0
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-16 14:15:33 +0200

    add rml mappings and workflow for eggnog

--- datasets/eggnog/download/download.sh
+++ datasets/eggnog/download/download.sh
@@ -4,12 +4,16 @@
 
 # http://eggnogdb.embl.de/#/app/downloads
 
-wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/d2s-scripts-repository/master/resources/eggnog-sample/NOG.annotations.tsv.gz
-
-wget -N https://raw.githubusercontent.com/MaastrichtU-IDS/d2s-scripts-repository/master/resources/eggnog-sample/NOG.members.tsv.gz
+wget -N http://eggnogdb.embl.de/download/latest/data/NOG/NOG.members.tsv.gz
+wget -N http://eggnogdb.embl.de/download/latest/data/NOG/NOG.annotations.tsv.gz
 
 find . -name "*.gz" -exec gzip -d  {} +
 
 # Add columns name
 sed -i '1s/^/TaxonomicLevel\tGroupName\tProteinCount\tSpeciesCount\tCOGFunctionalCategory\tProteinIDs\n/' NOG.members.tsv
 sed -i '1s/^/TaxonomicLevel\tGroupName\tProteinCount\tSpeciesCount\tCOGFunctionalCategory\tConsensusFunctionalDescription\n/' NOG.annotations.tsv
+
+
+# Convert TSV to CSV for RML
+sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' -e 's/,/|/g' NOG.members.tsv > NOG.members.csv
+sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' NOG.annotations.tsv > NOG.annotations.csv
