7aaed5b82f4f1ad19b3e606ea99171de83224216
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-16 13:07:31 +0200

    improve date ddl

--- datasets/date/download/download.sh
+++ datasets/date/download/download.sh
@@ -6,7 +6,10 @@ wget -N http://tatonettilab.org/resources/DATE/date_resource.zip
 # All in same dir
 unzip -o \*.zip
 
-rm date_resource/Drug_target_reactome_pathway_filtered.tsv
+# Convert TSV to CSV
+sed -e 's/"/\\"/g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/'  -e 's/\r//' date_resource/Drug_target_reactome_pathway.tsv > date.csv
+
+# rm date_resource/Drug_target_reactome_pathway_filtered.tsv
 # Should contains 2 tsv
 # date_resource/Drug_target_reactome_pathway.tsv
 # date_resource/Drug_target_reactome_pathway_filtered.tsv
