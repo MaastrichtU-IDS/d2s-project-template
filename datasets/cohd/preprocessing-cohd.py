1d9cce6d865f6066e745ed6fb42968997423dcdc
Vincent Emonet <vincent.emonet@gmail.com>
2020-11-11 17:13:13 +0100

    add 2 files to cohd

--- datasets/cohd/preprocessing-cohd.py
+++ datasets/cohd/preprocessing-cohd.py
@@ -1,22 +1,27 @@
 import csv
 
-# Set the right BioLink types in col2 domain_id
-with open('download/concepts.csv', 'r') as input_file, open('download/concepts_curated.csv','w',newline='') as output_file:
-   reader = csv.reader(input_file, delimiter=',')
-   writer = csv.writer(output_file, delimiter=',')
+preprocess_files = [{'name': 'domain_pair_concept_counts', 'col': [1,2]},
+                { 'name': 'domain_concept_counts', 'col': [1] },
+                { 'name': 'concepts', 'col': [2] } ]
 
-   for row in reader:
+for file in preprocess_files:
+    # Set the right BioLink types in col2 domain_id
+    with open('download/' + file['name'] + '.csv', 'r') as input_file, open('download/' + file['name'] + '_curated.csv','w',newline='') as output_file:
+        reader = csv.reader(input_file, delimiter=',')
+        writer = csv.writer(output_file, delimiter=',')
 
-        row[2] = row[2].replace('Condition','Disease')
-        # bl:DiseaseOrPhenotypicFeature ?
-        row[2] = row[2].replace('Observation','ActivityAndBehavior') # Not sure
-        row[2] = row[2].replace('Measurement','QuantityValue') # Not sure (has_numeric_value, has_unit)
-        row[2] = row[2].replace('Procedure','Procedure')
-        row[2] = row[2].replace('Device','Device')
-        row[2] = row[2].replace('Gender','PopulationOfIndividualOrganisms') # bl:BiologicalSex is an Attribute
-        row[2] = row[2].replace('Race','PopulationOfIndividualOrganisms')
-        row[2] = row[2].replace('Ethnicity','PopulationOfIndividualOrganisms')
-        # TODO: add bl:in_taxon human?
-        row[2] = row[2].replace('Relationship','Phenomenon')
+        for row in reader:
+            for col in file['col']:
+                row[col] = row[col].replace('Condition','Disease')
+                # bl:DiseaseOrPhenotypicFeature ?
+                row[col] = row[col].replace('Observation','ActivityAndBehavior') # Not sure
+                row[col] = row[col].replace('Measurement','QuantityValue') # Not sure (has_numeric_value, has_unit)
+                row[col] = row[col].replace('Procedure','Procedure')
+                row[col] = row[col].replace('Device','Device')
+                row[col] = row[col].replace('Gender','PopulationOfIndividualOrganisms') # bl:BiologicalSex is an Attribute
+                row[col] = row[col].replace('Race','PopulationOfIndividualOrganisms')
+                row[col] = row[col].replace('Ethnicity','PopulationOfIndividualOrganisms')
+                # TODO: add bl:in_taxon human?
+                row[col] = row[col].replace('Relationship','Phenomenon')
 
-        writer.writerow(row)
+            writer.writerow(row)
