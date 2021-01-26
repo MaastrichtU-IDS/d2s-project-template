e954a194fdf744eabde3929fd2c14165f98b7c58
Vincent Emonet <vincent.emonet@gmail.com>
2021-01-26 11:35:37 +0100

    update hgnc

--- datasets/hgnc/download/download.sh
+++ datasets/hgnc/download/download.sh
@@ -3,12 +3,12 @@
 ## Download sample TSV files from GitHub (OMOP CDM mappings, about 15M)
 wget -N ftp://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
 
-# Convert TSV to CSV for RMLStreamer
+# Convert TSV to CSV for RML Mapper
 sed -e 's/"//g' -e 's/\t/","/g' -e 's/^/"/' -e 's/$/"/' -e 's/\r//' hgnc_complete_set.txt > hgnc.csv
 
+##Example to run quick python scripts:
 
 # pip install pandas
-
 # python3 <<HEREDOC
 # import pandas as pd
 # csv_table=pd.read_table('hgnc_complete_set.tsv',sep='\t')
